b820902b1b1fa7df492fb7a7fc319a607bd1e65b
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-02-09 14:25:53 -0800

    Added FlowLayoutPanel container to wrap controls

--- DynamicControls/Form1.Designer.cs
+++ DynamicControls/Form1.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.btnCreateButtons = new System.Windows.Forms.Button();
+            this.flpDynamicControls = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
             // btnCreateButtons
             // 
-            this.btnCreateButtons.Location = new System.Drawing.Point(333, 187);
+            this.btnCreateButtons.Location = new System.Drawing.Point(327, 354);
             this.btnCreateButtons.Name = "btnCreateButtons";
             this.btnCreateButtons.Size = new System.Drawing.Size(133, 64);
             this.btnCreateButtons.TabIndex = 0;
@@ -41,11 +42,19 @@
             this.btnCreateButtons.UseVisualStyleBackColor = true;
             this.btnCreateButtons.Click += new System.EventHandler(this.btnCreateButtons_Click);
             // 
+            // flpDynamicControls
+            // 
+            this.flpDynamicControls.Location = new System.Drawing.Point(12, 12);
+            this.flpDynamicControls.Name = "flpDynamicControls";
+            this.flpDynamicControls.Size = new System.Drawing.Size(776, 336);
+            this.flpDynamicControls.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flpDynamicControls);
             this.Controls.Add(this.btnCreateButtons);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,5 +65,6 @@
         #endregion
 
         private Button btnCreateButtons;
+        private FlowLayoutPanel flpDynamicControls;
     }
 }
--- DynamicControls/Form1.cs
+++ DynamicControls/Form1.cs
@@ -8,20 +8,23 @@ namespace DynamicControls
         }
 
         int dynamicButtonNumber = 1;
+        Point currentPosition = new Point(0, 0);
 
         private void btnCreateButtons_Click(object sender, EventArgs e)
         {
+            const int ButtonWidth = 200;
+
             Button dynamicButton = new()
             {
                 Text = "This button was generated!",
-                Size = new Size(200, 50),
-                AutoSizeMode = AutoSizeMode.GrowAndShrink
+                Size = new Size(ButtonWidth, 50),
+                Location = new Point(ButtonWidth * (dynamicButtonNumber - 1), 0)
             };
 
             dynamicButton.Click += DynamicButton_Click;
             dynamicButton.Tag = "Button #" + dynamicButtonNumber++;
 
-            this.Controls.Add(dynamicButton);
+            flpDynamicControls.Controls.Add(dynamicButton);
         }
 
         private void DynamicButton_Click(object? sender, EventArgs e)
@@ -29,7 +32,7 @@ namespace DynamicControls
             Button clickedButton = sender as Button;
             MessageBox.Show("You clicked the dynamic button " + clickedButton.Tag);
 
-            this.Controls.Remove(clickedButton);
+            // this.Controls.Remove(clickedButton);
         }
     }
 }
