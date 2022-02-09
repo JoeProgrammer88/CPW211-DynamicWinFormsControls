4d33d690daced2285efef13a655e43c17a45201d
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-02-09 14:58:19 -0800

    Implementing dynamic group control example

--- DynamicControls/Form1.Designer.cs
+++ DynamicControls/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.btnCreateButtons = new System.Windows.Forms.Button();
             this.flpDynamicControls = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnGenerateControlGroup = new System.Windows.Forms.Button();
+            this.btnExtractControlGroupData = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCreateButtons
             // 
-            this.btnCreateButtons.Location = new System.Drawing.Point(327, 354);
+            this.btnCreateButtons.Location = new System.Drawing.Point(303, 192);
             this.btnCreateButtons.Name = "btnCreateButtons";
             this.btnCreateButtons.Size = new System.Drawing.Size(133, 64);
             this.btnCreateButtons.TabIndex = 0;
@@ -46,14 +48,36 @@
             // 
             this.flpDynamicControls.Location = new System.Drawing.Point(12, 12);
             this.flpDynamicControls.Name = "flpDynamicControls";
-            this.flpDynamicControls.Size = new System.Drawing.Size(776, 336);
+            this.flpDynamicControls.Size = new System.Drawing.Size(776, 174);
             this.flpDynamicControls.TabIndex = 1;
+            // 
+            // btnGenerateControlGroup
+            // 
+            this.btnGenerateControlGroup.Location = new System.Drawing.Point(303, 262);
+            this.btnGenerateControlGroup.Name = "btnGenerateControlGroup";
+            this.btnGenerateControlGroup.Size = new System.Drawing.Size(133, 64);
+            this.btnGenerateControlGroup.TabIndex = 2;
+            this.btnGenerateControlGroup.Text = "Generate Control Group";
+            this.btnGenerateControlGroup.UseVisualStyleBackColor = true;
+            this.btnGenerateControlGroup.Click += new System.EventHandler(this.btnGenerateControlGroup_Click);
+            // 
+            // btnExtractControlGroupData
+            // 
+            this.btnExtractControlGroupData.Location = new System.Drawing.Point(515, 229);
+            this.btnExtractControlGroupData.Name = "btnExtractControlGroupData";
+            this.btnExtractControlGroupData.Size = new System.Drawing.Size(131, 77);
+            this.btnExtractControlGroupData.TabIndex = 3;
+            this.btnExtractControlGroupData.Text = "Extract Control Group Data";
+            this.btnExtractControlGroupData.UseVisualStyleBackColor = true;
+            this.btnExtractControlGroupData.Click += new System.EventHandler(this.btnExtractControlGroupData_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 597);
+            this.Controls.Add(this.btnExtractControlGroupData);
+            this.Controls.Add(this.btnGenerateControlGroup);
             this.Controls.Add(this.flpDynamicControls);
             this.Controls.Add(this.btnCreateButtons);
             this.Name = "Form1";
@@ -66,5 +90,7 @@
 
         private Button btnCreateButtons;
         private FlowLayoutPanel flpDynamicControls;
+        private Button btnGenerateControlGroup;
+        private Button btnExtractControlGroupData;
     }
 }
--- DynamicControls/Form1.cs
+++ DynamicControls/Form1.cs
@@ -34,5 +34,81 @@ namespace DynamicControls
 
             // this.Controls.Remove(clickedButton);
         }
+
+        List<Panel> controlGroupPanels = new List<Panel>();
+
+        private void btnGenerateControlGroup_Click(object sender, EventArgs e)
+        {
+            // Panel container for holding a group of controls
+            Panel controlPanel = new Panel();
+            controlPanel.Size = new Size(500, 75);
+            controlPanel.Location = new Point(0, 350);
+            controlGroupPanels.Add(controlPanel);
+
+            this.Controls.Add(controlPanel);
+
+            // Populate first dynamic combo box
+            ComboBox foodOptions = new ComboBox();
+            List<string> allFoodOptions = GetFoodOptions();
+            foreach(string option in allFoodOptions)
+            {
+                foodOptions.Items.Add(option);
+            }
+            foodOptions.SelectedIndexChanged += FoodOptions_SelectedIndexChanged;
+
+            ComboBox toppings = new ComboBox();
+
+            // Add 2nd dropdown in tag, to keep reference to associated combobox
+            foodOptions.Tag = toppings; 
+
+            TextBox test = new TextBox();
+            test.Size = new Size(100, 50);
+            test.Location = new Point(200, 0);
+
+            controlPanel.Controls.Add(test);
+            controlPanel.Controls.Add(foodOptions);
+        }
+
+        private void FoodOptions_SelectedIndexChanged(object? sender, EventArgs e)
+        {
+            ComboBox currFoodOptionsBox = sender as ComboBox;
+            ComboBox toppingsOptions = currFoodOptionsBox.Tag as ComboBox;
+
+            List<string> toppings = GetToppingsForFood(currFoodOptionsBox.SelectedItem);
+        }
+
+        private List<string> GetToppingsForFood(object selectedItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Populating combobox with data. Data could come from
+        /// DB or another source
+        /// </summary>
+        /// <param name="foodOptions"></param>
+        private List<string> GetFoodOptions()
+        {
+            List<string> foodOptions = new List<string>
+            {
+                "Hamburger",
+                "Pasta",
+                "Chili"
+            };
+            return foodOptions;
+        }
+
+        private void btnExtractControlGroupData_Click(object sender, EventArgs e)
+        {
+            // Loop through each Panel that contains grouped controls
+            foreach (Panel p in controlGroupPanels)
+            {
+                TextBox textBox = p.Controls[0] as TextBox;
+                MessageBox.Show(textBox.Text);
+
+                ComboBox foodOptions = p.Controls[1] as ComboBox;
+                MessageBox.Show(foodOptions.SelectedItem.ToString());
+            }
+        }
     }
 }
